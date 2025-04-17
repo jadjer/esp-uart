ec29f78ba86aa6163e25a004e7b60b19aa43c2e6
jadjer <jadjer@ya.ru>
2025-04-17 13:10:25 +0300

    Refactoring

---
+++ include/uart/UART.hpp
@@ -0,0 +1,11 @@
+//
+// Created by jadjer on 16.04.25.
+//
+
+#ifndef ETCU_UART_HPP
+#define ETCU_UART_HPP
+
+class UART {
+};
+
+#endif//ETCU_UART_HPP
---
+++ src/UART.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jadjer on 16.04.25.
+//
+
+#include "uart/UART.hpp"
